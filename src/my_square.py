ebbce2f1517fb7ede33d6d3fd4786d969716b261
Amiya Ranjan Barik <amiyaranjanbrk@gmail.com>
2020-01-03 20:51:50 +0530

    add documentation

--- src/my_square.py
+++ src/my_square.py
@@ -1,6 +1,8 @@
 def my_square(a):
  """takes a value and returns its squared value
  uses ** operator
+ 
+ aaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbbb
  """
  return(a**2)
 print(my_square(10))
